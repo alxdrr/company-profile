7d04697fedb6f248838631392e4c403c9d817979
alxdrr <lexlex277@gmail.com>
2024-02-27 21:48:56 +0700

    add: faq section in faq page

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -1,12 +1,14 @@
 import NavigationBar from '../components/navbar-light.jsx';
 import Hero from '../components/faq-hero.jsx';
 import Footer from '../components/footer-2.jsx';
+import Faq from '../components/faq-faq.jsx';
 
 function FaqPage() {
   return (
     <>
       <NavigationBar />
       <Hero />
+      <Faq />
       <Footer />
     </>
   );
